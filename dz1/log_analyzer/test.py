8bf6ab8a2befc955eb02cac069b986767151cbe8
cidTomsk <61188943+cidTomsk@users.noreply.github.com>
2022-03-26 11:46:38 +0700

    Удаление тестов для несуществующих функций

--- dz1/log_analyzer/test.py
+++ dz1/log_analyzer/test.py
@@ -1,5 +1,5 @@
 import unittest
-from log_analyzer_reduced import parse_log_record, date_sort, median
+from log_analyzer_reduced import parse_log_record
 
 
 class TestUtilDate(unittest.TestCase):
@@ -8,13 +8,6 @@ class TestUtilDate(unittest.TestCase):
                    b'"GET /api/1/campaigns/?id=984781 HTTP/1.1" 200 662 ' \
                    b'"-" "-" "-" "1498723554-4102637017-4708-9976726" "-" 1.163\n'
         self.assertEqual(parse_log_record(log_line), ('/api/1/campaigns/?id=984781', '1.200'))
-
-    def test_date_sort(self):
-        files = ['nginx-access-ui.log-20160630.gz', 'nginx-access-ui.log-20170630', 'nginx-access-ui.log-20170730']
-        self.assertEqual(date_sort(files), 'nginx-access-ui.log-20170730')
-
-    def test_median(self):
-        self.assertEqual(median([1, 4, 8, 16, 32]), 16)
 
 
 if __name__ == '__main__':
